a5391c81766228b60768a2d24c952e1c45044fcd
wrwolfie <168141926+wrwolfie@users.noreply.github.com>
2024-04-26 00:20:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730, 440, 570, 10];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
+user.logOn({"accountName": username, "password": password});
 user.on('loggedOn', () => {
 	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
 	user.setPersona(status);               
